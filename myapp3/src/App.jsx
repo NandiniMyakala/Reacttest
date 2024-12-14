a6840ba2de78b9fd8c0ce451803c684041049d0e
NandiniMyakala <myakalanandini6@gmail.com>
2024-12-14 23:04:59 +0530

    reacttest added

---
+++ myapp3/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Table from './components/Table'
+function App() {
+  return (
+  <div>
+    <Table/>
+    <img src="src/image1.jpg" height="300" width="300"/>
+   </div>
+  )
+}
+export default App
